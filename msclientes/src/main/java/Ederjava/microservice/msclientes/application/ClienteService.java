d8a552065c1bc88ecb8181edf4db9e0e3a41fcb0
Eder <edermartelinho@hotmail.com>
2023-02-26 16:37:02 -0300

    Criado Cliente;server;repository;controller

---
+++ msclientes/src/main/java/Ederjava/microservice/msclientes/application/ClienteService.java
@@ -0,0 +1,25 @@
+package Ederjava.microservice.msclientes.application;
+
+import Ederjava.microservice.msclientes.domain.Cliente;
+import Ederjava.microservice.msclientes.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+
+    private final ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente){
+        return repository.save(cliente);
+    }
+
+    public Optional<Cliente>getByCPF(String cpf){
+        return repository.findByCpf(cpf);
+    }
+}
---
+++ msclientes/src/main/java/Ederjava/microservice/msclientes/application/ClientesController.java
@@ -0,0 +1,14 @@
+package Ederjava.microservice.msclientes.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("clientes")
+public class ClientesController {
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
+}
